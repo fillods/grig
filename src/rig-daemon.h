a2f7910c6f0611d06a40741dd224a1d83242cd8d
Alexandru Csete <oz9aec@gmail.com>
2004-06-11 12:47:31 +0000

    Updated.

--- src/rig-daemon.h
+++ src/rig-daemon.h
@@ -33,6 +33,35 @@
 #define RIG_DAEMON_H 1
 
 
+/** \brief List of available commands.
+ *
+ * This enumeration lists the available commands that can be executed by the RIG daemon.
+ * These values should be used to define each daemon cycle.
+ */
+typedef enum {
+	RIG_CMD_NONE = 0,     /*!< Not command. Used to end a sequence of commands. */
+
+	RIG_CMD_GET_FREQ_1,   /*!< Command to acquire primary frequency from rig. */
+	RIG_CMD_SET_FREQ_1,   /*!< Command to set primary frequency. */
+	RIG_CMD_GET_FREQ_2,   /*!< Command to acquire secondary frequency from rig. */
+	RIG_CMD_SET_FREQ_2,   /*!< Command to set secondary frequency. */
+	RIG_CMD_GET_RIT,      /*!< Command to get current RIT value. */
+	RIG_CMD_SET_RIT,      /*!< Command to set new RIT value. */
+	RIG_CMD_GET_XIT,      /*!< Command to get current XIT value. */
+	RIG_CMD_SET_XIT,      /*!< Command to set new XIT value. */
+	RIG_CMD_GET_VFO,      /*!< Command to get currently active VFO. */      
+	RIG_CMD_SET_VFO,      /*!< Command to select new VFO. */
+	RIG_CMD_GET_PSTAT,    /*!< Command to read power status (mains pwr, ON/OFF/STDBY). */
+	RIG_CMD_SET_PSTAT,    /*!< Command to set new power status (ON/OFF/STDBY). */
+	RIG_CMD_GET_PTT,      /*!< Command to get the current PTT status. */
+	RIG_CMD_SET_PTT,      /*!< Command to set the current PTT status. */
+	RIG_CMD_GET_MODE,     /*!< Command to get the current mode and pass band width. */
+	RIG_CMD_SET_MODE,     /*!< Command to set the new mode and/or pass band width. */
+
+	RIG_CMD_NUMBER        /*!< Number of available commands. */
+} rig_cmd_t;
+
+
 void rig_daemon_start (int);
 void rig_daemon_stop  (void);
 
