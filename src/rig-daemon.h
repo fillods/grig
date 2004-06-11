c785ae5ef2db7107f45d1531171479cecbaade4a
Alexandru Csete <oz9aec@gmail.com>
2004-06-11 10:03:51 +0000

    Added files.

---
+++ src/rig-daemon.h
@@ -0,0 +1,40 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+    Grig:  Gtk+ user interface for the Hamradio Control Libraries.
+
+    Copyright (C)  2001-2004  Alexandru Csete.
+
+    Authors: Alexandru Csete <csete@users.sourceforge.net>
+
+    Comments, questions and bugreports should be submitted via
+    http://sourceforge.net/projects/groundstation/
+    More details can be found at the project home page:
+
+            http://groundstation.sourceforge.net/
+ 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+  
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+  
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the
+          Free Software Foundation, Inc.,
+	  59 Temple Place, Suite 330,
+	  Boston, MA  02111-1307
+	  USA
+*/
+#ifndef RIG_DAEMON_H
+#define RIG_DAEMON_H 1
+
+
+void rig_daemon_start (int);
+void rig_daemon_stop  (void);
+
+
+#endif
