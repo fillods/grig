5893dd96ba4ac93291aca15e0eba3c1376ec71ad
Alexandru Csete <oz9aec@gmail.com>
2004-08-05 12:22:07 +0000

    Created files. Contains conversion functions between hamlib mode and array index type. mode_to_index algorithm was buggy. Fixed.

---
+++ src/rig-utils.c
@@ -0,0 +1,116 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+    Grig:  Gtk+ user interface for the Hamradio Control Libraries.
+
+    Copyright (C)  2001-2004  Alexandru Csete.
+
+    Authors: Alexandru Csete <csete@users.sourceforge.net>
+
+    Comments, questions and bugreports should be submitted via
+    http://sourceforge.net/projects/groundstation/
+    More details can be found at the project home page:
+
+            http://groundstation.sourceforge.net/
+ 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+  
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+  
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the
+          Free Software Foundation, Inc.,
+	  59 Temple Place, Suite 330,
+	  Boston, MA  02111-1307
+	  USA
+*/
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+#include <gnome.h>
+#include "rig-utils.h"
+
+
+
+/** \brief Convert hamlib mode to index.
+ *  \param mode The hamlib mode (1,2,4,8,..)
+ *  \return The converted index value.
+ *  
+ * This function converts the hamlib mode (1,2,4,8,...) to an array
+ * index (0,1,2,3,...). It is done using the formula
+ *
+ *    index = rint[log(mode)/log(2)]
+ *
+ * \note Hamlib uses 0 for RIG_MODE_NONE which clearly has to be treated as
+ *       illegal input. In case of zero input, however, this function will
+ *       simply return 0 as if the input was 1. The same protection is done
+ *       for negative input values.
+ *
+ * \bug It would probably be much better if we used rmode_t for input param type.
+ */
+guint
+rig_utils_mode_to_index (gint mode)
+{
+	guint index;
+
+	/* if input is numerically sane, compute the index */
+	if (mode > 0) {
+//		index = (log (mode) / LOG2);
+		
+		index = 0;
+		
+//		g_print ("%d", mode);
+
+		while (mode > 1) {
+			mode = mode >> 1;
+			index++;
+		}
+
+//		g_print (" => %d\n", index);
+	}
+
+	/* otherwise just return 0 */
+	else {
+		index = 0;
+	}
+
+	return index;
+}
+
+
+/** \brief Convert array index to hamlib mode.
+ *  \param index An integer to convert.
+ *  \return The hamlib mode.
+ *
+ * This function converts an array index (0,1,2,3,...) to hamlib mode type
+ * (1,2,4,8,...). The conversion is done using the formula:
+ *
+ *     mode  = 1 << index
+ *
+ * and index has to be in the range [0..12]
+ *
+ * \bug 12 is the last mode (RIG_MODE_PKTFM) in hamlib 1.2; this may have to be
+ *      updated if the hamlib API changes!
+ *
+ * \bug It would probably be better if we returned rmode_t type instead of guint.
+ */
+guint
+rig_utils_index_to_mode (gint index)
+{
+	guint mode;
+
+	/* check numerical sanity of input */
+	if ((index >= 0) && (index <= 12)) {
+		mode = 1 << index;
+	}
+	else {
+		mode = 1;
+	}
+
+	return mode;
+}
