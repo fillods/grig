25992857c4f2a7fa3cbd2f63433e214c62b05db4
Alexandru Csete <oz9aec@gmail.com>
2004-11-10 08:34:27 +0000

    Added offset and level info widgets. Level widget must be redesigned using scrollbar to allow nice display in case ofmany levels.

--- src/rig-gui-info.c
+++ src/rig-gui-info.c
@@ -38,15 +38,18 @@
  */
 #include <gtk/gtk.h>
 #include <hamlib/rig.h>
-#include "rig-gui-info.h"
 #include "support.h"
-
+#include "rig-gui-info.h"
+#include "rig-gui-info-data.h"
 
 extern GtkWidget   *grigapp;    /* defined in main.c */
 extern RIG         *myrig;      /* define in rig-demon.c */
 
 
-static GtkWidget *rig_gui_info_create_header (void);
+/* subsystem containers */
+static GtkWidget *rig_gui_info_create_header         (void);
+static GtkWidget *rig_gui_info_create_offset_frame   (void);
+static GtkWidget *rig_gui_info_create_level_frame    (void);
 
 
 /** \brief Create info dialog.
@@ -73,9 +76,19 @@ rig_gui_info_run ()
 	gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dialog)->vbox),
 			    rig_gui_info_create_header (),
 			    FALSE, FALSE, 0);
+
 	gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dialog)->vbox),
 			    gtk_hseparator_new (),
 			    FALSE, FALSE, 10);
+
+	gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dialog)->vbox),
+			    rig_gui_info_create_offset_frame (),
+			    FALSE, FALSE, 10);
+
+	gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dialog)->vbox),
+			    rig_gui_info_create_level_frame (),
+			    FALSE, FALSE, 10);
+
 	gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dialog)->vbox),
 			    gtk_label_new ("rig caps to come ..."),
 			    FALSE, FALSE, 10);
@@ -175,3 +188,183 @@ rig_gui_info_create_header ()
 }
 
 
+
+/** \brief Create frame containing offset info.
+ *  \return Frame containing the widgets.
+ *
+ * This function creates the widgets to display various
+ * offset information. The container is made of a 3x2 table
+ * showing RIT, XIT and IF-Shift and is surrounded by a nice
+ * frame.
+ */
+static GtkWidget *
+rig_gui_info_create_offset_frame ()
+{
+	GtkWidget *frame;
+	GtkWidget *table;
+	GtkWidget *label;
+	gchar     *text;
+
+	table = gtk_table_new (3, 2, TRUE);
+
+	label = gtk_label_new (_("RIT:"));
+	gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+	gtk_table_attach (GTK_TABLE (table), label,
+			  0, 1, 0, 1,
+			  GTK_EXPAND | GTK_FILL,
+			  GTK_EXPAND | GTK_FILL,
+			  5, 0);
+
+	/* use UTF-8 code for plus/minus */
+	text = g_strdup_printf (_("\302\261%.2f kHz"),
+				((gfloat) myrig->caps->max_rit) / 1000.0);
+	label = gtk_label_new (text);
+	gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+	gtk_table_attach (GTK_TABLE (table), label,
+			  1, 2, 0, 1,
+			  GTK_EXPAND | GTK_FILL,
+			  GTK_EXPAND | GTK_FILL,
+			  5, 0);
+	g_free (text);
+
+	label = gtk_label_new (_("XIT:"));
+	gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+	gtk_table_attach (GTK_TABLE (table), label,
+			  0, 1, 1, 2,
+			  GTK_EXPAND | GTK_FILL,
+			  GTK_EXPAND | GTK_FILL,
+			  5, 0);
+
+	text = g_strdup_printf (_("\302\261%.2f kHz"),
+				((gfloat) myrig->caps->max_xit) / 1000.0);
+	label = gtk_label_new (text);
+	gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+	gtk_table_attach (GTK_TABLE (table), label,
+			  1, 2, 1, 2,
+			  GTK_EXPAND | GTK_FILL,
+			  GTK_EXPAND | GTK_FILL,
+			  5, 0);
+	g_free (text);
+
+	label = gtk_label_new (_("IF-SHIFT:"));
+	gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+	gtk_table_attach (GTK_TABLE (table), label,
+			  0, 1, 2, 3,
+			  GTK_EXPAND | GTK_FILL,
+			  GTK_EXPAND | GTK_FILL,
+			  5, 0);
+
+	text = g_strdup_printf (_("\302\261%.2f kHz"),
+				((gfloat) myrig->caps->max_ifshift) / 1000.0);
+	label = gtk_label_new (text);
+	gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+	gtk_table_attach (GTK_TABLE (table), label,
+			  1, 2, 2, 3,
+			  GTK_EXPAND | GTK_FILL,
+			  GTK_EXPAND | GTK_FILL,
+			  5, 0);
+	g_free (text);
+
+	frame = gtk_frame_new (_("Max. Offsets"));
+	gtk_frame_set_label_align (GTK_FRAME (frame), 0.5, 0.5);
+	gtk_container_add (GTK_CONTAINER (frame), table);
+
+	return frame;
+}
+
+
+
+/** \brief Create frame containing levels info.
+ *  \return Frame containing the widgets.
+ *
+ * This function creates the widgets to display various
+ * level settings information. The container is made of a 2x2 table
+ * showing SET and GET levels and it is surrounded by a nice
+ * frame.
+ *
+ *  REWRITE: vertical scrollbar!
+ *
+ *             READ    WRITE
+ *
+ *  LEVEL 1     X        X
+ *  LEVEL 2     X
+ */
+static GtkWidget *
+rig_gui_info_create_level_frame    ()
+{
+	GtkWidget *frame;
+	GtkWidget *table;
+	GtkWidget *label;
+	gchar     *text;
+	gchar     *buff;
+	setting_t  levels;
+	guint      i;
+	guint      count = 0;
+
+	table = gtk_table_new (2, 2, FALSE);
+
+	label = gtk_label_new (_("READ:"));
+	gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+	gtk_table_attach (GTK_TABLE (table), label,
+			  0, 1, 0, 1,
+			  GTK_EXPAND | GTK_FILL,
+			  GTK_EXPAND | GTK_FILL,
+			  5, 0);
+
+	/* get read levels */
+	text = g_strdup (" ");
+	levels = rig_has_get_level (myrig, 0xFFFFFF);
+	
+	/* loop over all values */
+	for (i = 0; i < 31; i++) {
+
+		/* if we have level append it to string */
+		if (levels & (1 << i)) {
+
+			buff = g_strdup_printf ("%s %s", text, RIG_LEVEL_STR[i]);
+			count++;
+
+			g_free (text);
+			text = g_strdup (buff);
+			g_free (buff);
+		}
+
+		if (count >= 5) {
+			buff = g_strdup_printf ("%s\n", text);
+			count = 0;
+
+			g_free (text);
+			text = g_strdup (buff);
+			g_free (buff);
+		}
+
+
+	}
+
+	label = gtk_label_new (text);
+	gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+	gtk_table_attach (GTK_TABLE (table), label,
+			  1, 2, 0, 1,
+			  GTK_EXPAND | GTK_FILL,
+			  GTK_EXPAND | GTK_FILL,
+			  5, 0);
+	g_free (text);
+
+	label = gtk_label_new (_("WRITE:"));
+	gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+	gtk_table_attach (GTK_TABLE (table), label,
+			  0, 1, 1, 2,
+			  GTK_EXPAND | GTK_FILL,
+			  GTK_EXPAND | GTK_FILL,
+			  5, 0);
+
+	/* get write levels */
+	levels = rig_has_set_level (myrig, 0xFFFFFF);
+
+
+	frame = gtk_frame_new (_("Levels"));
+	gtk_frame_set_label_align (GTK_FRAME (frame), 0.5, 0.5);
+	gtk_container_add (GTK_CONTAINER (frame), table);
+
+	return frame;
+}
