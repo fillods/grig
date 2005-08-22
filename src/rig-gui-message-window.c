8c225e190111c0a58e005ad24e3d95f6b1932e8d
Alexandru Csete <oz9aec@gmail.com>
2005-08-22 10:57:20 +0000

    Added files.

---
+++ src/rig-gui-message-window.c
@@ -0,0 +1,108 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+    Grig:  Gtk+ user interface for the Hamradio Control Libraries.
+
+    Copyright (C)  2001-2005  Alexandru Csete.
+
+    Authors: Alexandru Csete <csete@users.sourceforge.net>
+
+    Comments, questions and bugreports should be submitted via
+    http://sourceforge.net/projects/groundstation/
+    More details can be found at the project home page:
+
+            http://groundstation.sourceforge.net/
+ 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+  
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+  
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the
+          Free Software Foundation, Inc.,
+	  59 Temple Place, Suite 330,
+	  Boston, MA  02111-1307
+	  USA
+*/
+#include <gtk/gtk.h>
+#include "rig-gui-message-window.h"
+
+
+static gboolean visible     = FALSE;   /* Is message window visible? */
+static gboolean initialised = FALSE;   /* Is module initialised? */
+
+
+
+/* Initialise message window.
+ *
+ * This function creates the message window and allocates all the internal
+ * data structures. The function should be called when the main program
+ * is initialised.
+ */
+void
+rig_gui_message_window_init  ()
+{
+	if (!initialised) {
+
+		/* do some init stuff */
+
+		initialised = TRUE;
+	}
+}
+
+
+/* Clean up message window.
+ *
+ * This function cleans up the message window by freeing the allocated
+ * memory. It should be called when the main program exits.
+ *
+ * Note: It is not strictly neccessary to call this function, since it
+ *       is also invoked by the 'destroy' callback of the window.
+ *
+ * FIXME: In the above case this function is not necessary at all.
+ */
+void
+rig_gui_message_window_clean ()
+{
+	if (initialised) {
+
+		if (visible) {
+			rig_gui_message_window_hide ();
+		}
+
+		/* do some cleaning stuff */
+
+		initialised = FALSE;
+	}
+}
+
+
+
+void
+rig_gui_message_window_show ()
+{
+	if (!initialised)
+		rig_gui_message_window_init ();
+
+	if (!visible) {
+		g_print ("Show window\n");
+
+		visible = TRUE;
+	}
+
+}
+
+
+void
+rig_gui_message_window_hide  ()
+{
+	if (visible) {
+		g_print ("Hide window\n");
+		visible = FALSE;
+	}
+}
