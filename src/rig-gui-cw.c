efa8ceee6e21659375ab77d962f7d5a33cf54022
Alexandru Csete <oz9aec@gmail.com>
2006-07-01 09:47:26 +0000

    Added file for CW level controls.

---
+++ src/rig-gui-cw.c
@@ -0,0 +1,50 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+    Grig:  Gtk+ user interface for the Hamradio Control Libraries.
+
+    Copyright (C)  2001-2006  Alexandru Csete.
+
+    Authors: Alexandru Csete <csete@users.sourceforge.net>
+
+    Comments, questions and bugreports should be submitted via
+    http://sourceforge.net/projects/groundstation/
+    More details can be found at the project home page:
+
+            http://groundstation.sourceforge.net/
+ 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+  
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+  
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, visit http://www.fsf.org/
+ 
+ 
+ 
+ 
+*/
+#include <gtk/gtk.h>
+#include <glib/gi18n.h>
+#include <hamlib/rig.h>
+#include "rig-data.h"
+#include "rig-utils.h"
+#include "rig-gui-cw.h"
+
+
+/** \brief Create level controls.
+ *  \return A container widget containing the controls.
+ *
+ * This function creates and initializes the level controls for grig.
+ * The controls are hidden by default using a GtkExpander widget.
+ */
+void
+rig_gui_cw_create ()
+{
+
+}
