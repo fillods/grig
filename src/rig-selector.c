55c3c90c2ad4ce16ef65145e32c31afbd7622e67
Alexandru Csete <oz9aec@gmail.com>
2007-09-16 21:14:26 +0000

    Reaarranged buttons in rig selector window.

--- src/rig-selector.c
+++ src/rig-selector.c
@@ -40,6 +40,11 @@
 /* private function declarations */
 static gint rig_selector_delete     (GtkWidget *, GdkEvent *, gpointer);
 static void rig_selector_destroy    (GtkWidget *, gpointer);
+static void rig_selector_new_cb     (GtkWidget *, gpointer);
+static void rig_selector_del_cb     (GtkWidget *, gpointer);
+static void rig_selector_edit_cb    (GtkWidget *, gpointer);
+static void rig_selector_cancel_cb  (GtkWidget *, gpointer);
+static void rig_selector_connect_cb (GtkWidget *, gpointer);
 
 
 
@@ -61,44 +66,74 @@ static void rig_selector_destroy    (GtkWidget *, gpointer);
 gchar *
 rig_selector_execute ()
 {
-    GtkWidget *window;   /* the main rig-selector window */
-    gchar     *icon;     /* window icon file name */
-    GtkWidget *vbox;     /* the main vertical box in the window */
-    GtkWidget *butbox;   /* the button box in the bottom of the window */
-    GtkWidget *conbut;   /* Connect button */
-    GtkWidget *cancbut;  /* Cancel button */
-    GtkWidget *addbut;   /* New button */
-    GtkWidget *editbut;  /* Edit button */
-    GtkWidget *delbut;   /* delete button */
+    GtkWidget   *window;   /* the main rig-selector window */
+    gchar       *icon;     /* window icon file name */
+    GtkWidget   *vbox;     /* the main vertical box in the window */
+    GtkWidget   *butbox1;  /* The button box with New, edit, and delete butons */
+    GtkWidget   *butbox2;  /* the button box in the bottom of the window */
+    GtkWidget   *conbut;   /* Connect button */
+    GtkWidget   *cancbut;  /* Cancel button */
+    GtkWidget   *newbut;   /* New button */
+    GtkWidget   *editbut;  /* Edit button */
+    GtkWidget   *delbut;   /* delete button */
+    GtkTooltips *tips;
 
 
+    tips = gtk_tooltips_new ();
 
     /* connect button */
     conbut = gtk_button_new_from_stock (GTK_STOCK_CONNECT);
+    gtk_widget_set_sensitive (conbut, FALSE);
+    gtk_tooltips_set_tip (tips, conbut,
+                          _("Connect to the selected radio."),
+                          _("Grig will attempt to establish connection to the "
+                            "selected radio using the specified settings. If "
+                            "the connection is successful, the main application "
+                            "window will be loaded."));
     
     /* cancel button */
     cancbut = gtk_button_new_from_stock (GTK_STOCK_CANCEL);
+    gtk_tooltips_set_tip (tips, cancbut,
+                          _("Cancel radio selection."),
+                          _("Cancel radio selection. This will end grig."));
     
     /* add nutton */
-    addbut = gtk_button_new_from_stock (GTK_STOCK_ADD);
+    newbut = gtk_button_new_from_stock (GTK_STOCK_NEW);
+    gtk_tooltips_set_tip (tips, newbut,
+                          _("Add a new radio to the list."),
+                          _("A new configuration window will be shown allowing "
+                            "you to select a radio and specify the connection "
+                            "settings."));
     
     /* delete button */
     delbut = gtk_button_new_from_stock (GTK_STOCK_DELETE);
+    gtk_widget_set_sensitive (delbut, FALSE);
+    gtk_tooltips_set_tip (tips, delbut,
+                          _("Delete the currently selected radio."), NULL);
     
     /* edit button */
     editbut = gtk_button_new_from_stock (GTK_STOCK_EDIT);
+    gtk_widget_set_sensitive (editbut, FALSE);
+    gtk_tooltips_set_tip (tips, editbut,
+                          _("Edit the settings for the currently selected radio."),
+                            NULL);
     
     /* button box*/
-    butbox = gtk_hbutton_box_new ();
-    gtk_container_add (GTK_CONTAINER (butbox), addbut);
-    gtk_container_add (GTK_CONTAINER (butbox), editbut);
-    gtk_container_add (GTK_CONTAINER (butbox), delbut);
-    gtk_container_add (GTK_CONTAINER (butbox), cancbut);
-    gtk_container_add (GTK_CONTAINER (butbox), conbut);
+    butbox1 = gtk_hbutton_box_new ();
+    gtk_button_box_set_layout (GTK_BUTTON_BOX (butbox1), GTK_BUTTONBOX_START);
+    butbox2 = gtk_hbutton_box_new ();
+    gtk_button_box_set_layout (GTK_BUTTON_BOX (butbox2), GTK_BUTTONBOX_END);
+    gtk_box_set_spacing (GTK_BOX (butbox2), 10);
+    gtk_container_add (GTK_CONTAINER (butbox1), newbut);
+    gtk_container_add (GTK_CONTAINER (butbox1), editbut);
+    gtk_container_add (GTK_CONTAINER (butbox1), delbut);
+    gtk_container_add (GTK_CONTAINER (butbox2), cancbut);
+    gtk_container_add (GTK_CONTAINER (butbox2), conbut);
     
     /* vertical box */
     vbox = gtk_vbox_new (FALSE, 10);
-    gtk_box_pack_end (GTK_BOX (vbox), butbox, FALSE, FALSE, 0);
+    gtk_box_pack_start (GTK_BOX (vbox), butbox1, FALSE, FALSE, 0);
+    gtk_box_pack_end (GTK_BOX (vbox), butbox2, FALSE, FALSE, 0);
     gtk_box_pack_end (GTK_BOX (vbox), gtk_hseparator_new (), FALSE, FALSE, 0);
 
     /* create window */
